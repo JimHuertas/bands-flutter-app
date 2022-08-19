260b5b49a26fbcc96a29a730b6d5c2a594a69780
JimHuertas <live.land18@gmail.com>
2022-08-19 13:19:17 -0500

    Adding macos desktop support, changing to original name the flutter project (got some problems with the newest)

--- 01-band-names-servers/sockets/socket.js
+++ 01-band-names-servers/sockets/socket.js
@@ -1,11 +1,26 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon jovi'));
+bands.addBand(new Band('$uicideboy$'));
+bands.addBand(new Band('Ghostemane'));
 
 //Mensaje de Sockets
 io.on('connection', client => {
     console.log('Cliente Conectado');
 
+    client.emit('bandas-activas', bands.getBands());
+
     client.on('disconnect', () => { 
         console.log('Cliente desconectado');
+    });
+
+    client.on('add-votes', (payload) => {
+        
+        bands.voteBand(payload['id'])
     });
 
     client.on('mensaje', (payload) => {
